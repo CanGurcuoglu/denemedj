c86ae46baceeea7aa92abc6cd0b606d97af46276
Can Gürcüoğlu <122644441+CanGurcuoglu@users.noreply.github.com>
2023-10-11 00:39:01 +0300

    Delete postgresTest directory

--- postgresTest/missions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('missions/', views.MissionListCreateView.as_view(), name='mission-list-create'),
-    path('missions/<int:pk>/', views.MissionDetailView.as_view(), name='mission-detail'),
-]
